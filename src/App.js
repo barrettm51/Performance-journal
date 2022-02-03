436ec89c4d37c7ea8f21c20e89b60338333820bd
barrettm51 <32210357+barrettm51@users.noreply.github.com>
2022-02-03 12:37:23 -0800

    Implement React Router. For now, the default path brings you straight to the journal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import CurrentJournalEntry from './Components/journal/currentJournalEntry/CurrentJournalEntry';
 import JournalPage from './Components/journal/JournalPage';
 import Nav from './Components/Nav';
 import AccountSettings from './Components/AccountSettings/AccountSettings';
 import Dashboard from './Components/Dashboard/Dashboard';
+import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div /*className="App"*/>
-      <Nav />
-      
-      <JournalPage />
-    </div>
+    <BrowserRouter>
+      <div >
+        <Nav />
+        <Routes>
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/" element={<JournalPage />} />
+            <Route path="/AccountSettings" element={<AccountSettings />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
+    const navStyle = {
+        color: 'white'
+    };
+    
     return(
         <nav>
-            <h3>Logo</h3>
-            <ul>
-                <li>Dashboard</li>
-                <li>Journal</li>
-                <li>Account Settings</li>
+            <h3>Performance Journal</h3>
+            <ul className='nav-links' >
+                <Link style={navStyle} to='/Dashboard'>
+                    <li>Dashboard</li>
+                </Link>
+                <Link style={navStyle} to='/'>
+                    <li>Journal</li>
+                </Link>
+                <Link style={navStyle} to='/AccountSettings'>
+                    <li>Account Settings</li>
+                </Link>
             </ul>
         </nav>
     );
