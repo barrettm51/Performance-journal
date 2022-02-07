bfa1ace954a4a34ac5499bc4ec7cf7233a2dc95b
barrettm51 <32210357+barrettm51@users.noreply.github.com>
2022-02-07 14:46:31 -0800

    Remove Delete button until database is connected

--- src/Components/journal/currentJournalEntry/CurrentJournalEntry.js
+++ src/Components/journal/currentJournalEntry/CurrentJournalEntry.js
@@ -51,7 +51,7 @@ export default function CurrentJournalEntry({journalId}) {
                 <p id='date-created'>Entry created: {journalEntries[journalId].journalDateCreated}</p>
                 <p id='last-modified'>Last modified: {journalEntries[journalId].journalLastModified}</p>
                 <br></br>
-                <button onClick={deleteEntry} >Delete</button>
+                {/* <button onClick={deleteEntry} >Delete</button> To be implemented once the database is up and running*/} 
             </form>
         </div>
     );
