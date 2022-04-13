70d9b4a5823cafb75486eaa8c882b470f9e9ddcf
barrettm51 <32210357+barrettm51@users.noreply.github.com>
2022-04-12 17:28:42 -0700

    uninstall some unneeded dependencies and add some text to Dashboard page

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ export default function Dashboard() {
     return(
         <div>
             <h1>Dashboard</h1>
+            <h2> Welcome to your Performance journal </h2>
         </div>
     );
 };
