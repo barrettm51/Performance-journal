dd83d30ab08b8fc2a55e08646f05b0898ea15768
barrettm51 <32210357+barrettm51@users.noreply.github.com>
2022-05-25 21:02:22 -0700

    change nav bar to have different appearance when there is/ isnt a session. Add private route component- in progress

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -4,19 +4,23 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useStytchSession } from '@stytch/stytch-react';
 
 
-function Navigation() {    
+function Navigation() {
+    const session = useStytchSession();
+    
     return(
         <Navbar collapseOnSelect fixed='top' expand='sm' variant='dark' className='navbar'>
             <Container className="navbar-container" >
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                 <Navbar.Collapse id='responsive-navbar-nav' >
                     <Nav>
-                        <Navbar.Brand as={Link} to='/Dashboard'>Performance Journal</Navbar.Brand>
-                        <Nav.Link as={Link} to='/Dashboard'>Dashboard</Nav.Link>
-                        <Nav.Link as={Link} to='/Journals'>Journal</Nav.Link>
-                        <Nav.Link as={Link} to='/AccountSettings'>Account Settings</Nav.Link>
-                        <Nav.Link as={Link} to='/Login'>Login</Nav.Link>
-                        <button>Logout</button>
+                        {session && <Navbar.Brand as={Link} to='/Dashboard'>Performance Journal</Navbar.Brand>}
+                        {session && <Nav.Link as={Link} to='/Dashboard'>Dashboard</Nav.Link>}
+                        {session && <Nav.Link as={Link} to='/Journals'>Journal</Nav.Link>}
+                        {session && <Nav.Link as={Link} to='/AccountSettings'>Account Settings</Nav.Link>}
+                        {session && <button>Logout</button>}
+
+                        {!session && <Navbar.Brand as={Link} to='/Login'>Performance Journal</Navbar.Brand>}
+                        {!session && <Nav.Link as={Link} to='/Login'>Login</Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
